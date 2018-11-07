79f68219fc6f8781e1fb1f85c8dcceaae11f4c00
Noi-Git <noi.patsin@gmail.com>
2018-11-07 11:37:04 -0800

    Completed modal styling and functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -101,6 +101,7 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import $ from 'jquery'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 
 var mobileMenu = new MobileMenu()
@@ -110,3 +111,5 @@ new RevealOnScroll($(".feature-item"), "90%")
 new RevealOnScroll($(".testimonial"), "80%")
 
 var stickyHeader = new StickyHeader()
+
+var modal = new Modal()
